941f6a13b903039ae23dc65fe1d3db2ae1389909
Felipe Sakuma <fycorsak@gmail.com>
2022-06-27 14:09:18 -0300

    refactor: create layered structure for health route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,14 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import authRouter from './authRoutes.js';
 import categoriesRouter from './categoriesRoutes.js';
 import disciplinesRouter from './disciplinesRoutes.js';
 import testsRouter from './testsRoutes.js';
 import teachersRouter from './teachersRoutes.js';
+import healthRouter from './healthRoute.js';
 
 const router = Router();
 
-router.get('/health', (_req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.use(healthRouter);
 router.use(authRouter);
 router.use(testsRouter);
 router.use(categoriesRouter);
