e8839239e75c4656f17462e892c3d75591f3593e
Felipe Sakuma <fycorsak@gmail.com>
2022-06-27 14:08:53 -0300

    chore: add underline for unused vars

--- src/controllers/disciplinesController.ts
+++ src/controllers/disciplinesController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import * as services from '../services/disciplinesServices.js';
 
-export async function getDisciplines(req: Request, res: Response) {
+export async function getDisciplines(_req: Request, res: Response) {
   const disciplines = await services.getDisciplines();
 
   return res.send(disciplines);
