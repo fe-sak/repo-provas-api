5aed604e2daf1af5b763bcfd1d8352fea3f84dfe
Felipe Sakuma <fycorsak@gmail.com>
2022-06-30 14:28:19 -0300

    fix: fix heroku deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,9 @@ import { Router } from 'express';
 import authRouter from './authRoutes.js';
 import categoriesRouter from './categoriesRoutes.js';
 import disciplinesRouter from './disciplinesRoutes.js';
-import testsRouter from './testsRoutes.js';
-import teachersRouter from './teachersRoutes.js';
 import healthRouter from './healthRoute.js';
+import teachersRouter from './teachersRoutes.js';
+import testsRouter from './testsRoutes.js';
 
 const router = Router();
 
