d6b3daddd32805311e9f4b4ca719f5f788a5dcec
Felipe Sakuma <fycorsak@gmail.com>
2022-06-30 14:32:15 -0300

    chore: add console for debugging error

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -3,9 +3,10 @@ import { ErrorRequestHandler } from 'express';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
   if (error.statusCode) {
+    console.log(error);
     return res.sendStatus(error.statusCode);
   }
-
+  console.log(error);
   return res.sendStatus(500);
 };
 
